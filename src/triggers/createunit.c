717da0708266b88caaed72af6d991f755a5bbadf
Enno Rehling <enno.rehling@gmail.com>
2016-02-13 15:44:15 +0100

    remove global.data_version where gamedata.version is available github issue #479

--- src/triggers/createunit.c
+++ src/triggers/createunit.c
@@ -97,7 +97,7 @@ static int createunit_read(trigger * t, gamedata *data)
         read_reference(&td->f, data->store, read_faction_reference, resolve_faction);
     int rc =
         read_reference(&td->r, data->store, read_region_reference,
-        RESOLVE_REGION(global.data_version));
+        RESOLVE_REGION(data->version));
     td->race = (const struct race *)read_race_reference(data->store).v;
 
     if (uc == 0 && rc == 0) {
